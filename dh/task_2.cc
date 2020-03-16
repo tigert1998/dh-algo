d24f5e8c4b37af869f20d09533a73927f82351dd
Tang Xiaohu <v-xiat@microsoft.com>
2020-03-16 09:59:14 +0800

    Fix a bug

--- dh/task_2.cc
+++ dh/task_2.cc
@@ -2,15 +2,16 @@
 
 BigInteger p, g, a, b;
 
-BigInteger Power(const BigInteger &x, const BigInteger &t) {
+BigInteger Power(const BigInteger &x, const BigInteger &t,
+                 const BigInteger &mod) {
   if (t == BigInteger(0)) {
     return BigInteger(1);
   }
   auto div_res = t.Div(BigInteger(2));
-  auto ans = Power(x, *div_res.div);
-  ans = ans.Mul(ans);
+  auto ans = Power(x, *div_res.div, mod);
+  ans = *ans.Mul(ans).Div(mod).mod;
   if (!(*div_res.mod == BigInteger(0))) {
-    ans = ans.Mul(x);
+    ans = *ans.Mul(x).Div(mod).mod;
   }
   return ans;
 }
@@ -18,9 +19,9 @@ BigInteger Power(const BigInteger &x, const BigInteger &t) {
 int main() {
   try {
     std::cin >> p >> g >> a >> b;
-    std::cout << *Power(g, a).Div(p).mod << "\n";
-    std::cout << *Power(g, b).Div(p).mod << "\n";
-    std::cout << *Power(g, a.Mul(b)).Div(p).mod << "\n";
+    std::cout << Power(g, a, p) << "\n";
+    std::cout << Power(g, b, p) << "\n";
+    std::cout << Power(Power(g, a, p), b, p) << "\n";
   } catch (std::exception &e) {
     std::cout << e.what() << std::endl;
     exit(0);
